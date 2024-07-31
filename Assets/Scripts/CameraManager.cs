2584bb88de8d89b1c32385b4617fd3e5df1d108e
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-07-31 19:17:17 +0200

    set up everthing by removing the first person camera and adjusting the code so i can start implementing the multiplayer feature

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -3,54 +3,13 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     // Declaring and initializing variables.
-    public GameObject mainCam;
-    public GameObject firstPersonCam;
     public GameObject player;
-    private bool _isMainCamActive;
-    private readonly Vector3 _offsetFirstPerson = new (0, 2, 1);
     private readonly Vector3 _offsetMain = new (0, 5, -7);
-    
-    
-    private void Start()
-    {
-        // Setting the Status for the cameras at the start of the game.
-        mainCam.SetActive(true);
-        firstPersonCam.SetActive(false);
-        _isMainCamActive = true;
-    }
-    
-    private void Update()
-    {
-        // With the key input the SwitchCam Method is executed
-        if (Input.GetButtonDown("Camera Switch"))
-        {
-            SwitchCam();
-        }
-    }
     
     // LateUpdate is called after the Update method. Makes sure that the camera doesn't stutter.
     private void LateUpdate()
     {
         // Offsetting the main camera away from the players position by adding a new vector3 instance.
-        if (_isMainCamActive)
-        {
-            mainCam.transform.position = player.transform.position + _offsetMain;
-        }
-    }
-    
-    // Enables and disables the camera objects based on the current status of _isMainCamActive. Also changes said status.
-    private void SwitchCam()
-    {
-        if (_isMainCamActive)
-        {
-            mainCam.SetActive(false);
-            firstPersonCam.SetActive(true);
-        }
-        else
-        {
-            mainCam.SetActive(true);
-            firstPersonCam.SetActive(false);
-        }
-        _isMainCamActive = !_isMainCamActive;
+            transform.position = player.transform.position + _offsetMain;
     }
 }
