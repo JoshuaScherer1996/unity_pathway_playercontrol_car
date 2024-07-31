c4c0e2eaea25c13942cb5c31d92ab2649457c284
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-07-31 17:53:58 +0200

    restructured the code for less script redundancy and documented the finalized code

--- Assets/Scripts/AutomaticDrive.cs
+++ Assets/Scripts/AutomaticDrive.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class AutomaticDrive : MonoBehaviour
 {
+    // Declaring the constant.
     private const float Speed = 20.0f;
     
     private void Update()
     {
+        // Making the vehicle constantly drive forward based on the Speed constant and seconds.
         transform.Translate(Vector3.forward * (Time.deltaTime * Speed));
     }
 }
--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,35 +1,43 @@
 using UnityEngine;
 
-public class SwitchCamera : MonoBehaviour
+public class CameraManager : MonoBehaviour
 {
+    // Declaring and initializing variables.
     public GameObject mainCam;
     public GameObject firstPersonCam;
     public GameObject player;
     private bool _isMainCamActive;
-    private readonly Vector3 _offset = new (0, 2, 1);
+    private readonly Vector3 _offsetFirstPerson = new (0, 2, 1);
+    private readonly Vector3 _offsetMain = new (0, 5, -7);
+    
     
     private void Start()
     {
-        // Ensure the main camera is active at the start
+        // Setting the Status for the cameras at the start of the game.
         mainCam.SetActive(true);
         firstPersonCam.SetActive(false);
         _isMainCamActive = true;
     }
+    
     private void Update()
     {
+        // With the key input the SwitchCam Method is executed
         if (Input.GetButtonDown("Camera Switch"))
         {
             SwitchCam();
         }
     }
-
     
+    // LateUpdate is called after the Update method. Makes sure that the camera doesn't stutter.
     private void LateUpdate()
     {
-        firstPersonCam.transform.position = player.transform.position + _offset;
+        // Offsetting the camera away from the players position by adding  new vector3 instances.
+        mainCam.transform.position = player.transform.position + _offsetMain;
+        firstPersonCam.transform.position = player.transform.position + _offsetFirstPerson;
         firstPersonCam.transform.rotation = player.transform.rotation;
     }
     
+    // Enables and disables the camera objects based on the current status of _isMainCamActive. Also changes said status.
     private void SwitchCam()
     {
         if (_isMainCamActive)
