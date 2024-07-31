d422258e7cd244129d776f5f66ae8332e2eaf1e1
Joshua Scherer <62510842+JoshuaScherer1996@users.noreply.github.com>
2024-07-31 19:32:09 +0200

    finished the simple local multiplayer feature by including input ID's for the different vehicles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,14 @@ public class PlayerController : MonoBehaviour
     private const float TurnSpeed = 50.0f;
     private float _horizontalInput;
     private float _forwardInput;
+    public string inputID;
 
     // Update is called once per frame
     private void Update()
     {
         // Getting the Player Input from the horizontal and vertical Axis.
-        _horizontalInput = Input.GetAxis("Horizontal");
-        _forwardInput = Input.GetAxis("Vertical"); 
+        _horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        _forwardInput = Input.GetAxis("Vertical" + inputID); 
         
         // Moves the vehicle forward in relation to the input.
         transform.Translate(Vector3.forward * (Time.deltaTime * Speed * _forwardInput)); 
